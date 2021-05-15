ba641e64f95ceec8fb37f8149163d19dcc70d526
Mooneeb-Habib <43096155+Mooneeb-Habib@users.noreply.github.com>
2021-05-16 01:17:01 +0500

    User required to input extrusion amount

--- model2CVS/csvGenerator.m
+++ model2CVS/csvGenerator.m
@@ -1,12 +1,13 @@
 clear all; close all; clc;
 
 model = 'pentagon.stl';
-[allTargets, sz] = startSlicer(model);
+extrusionp = 350;
+[allTargets, sz] = startSlicer(model,extrusionp);
 
 %Removing all the duplicate coordinates that enter the system
-Z0 = unique(allTargets{1},'rows'); 
-Z1 = unique(allTargets{2},'rows');
-Z2 = unique(allTargets{3},'rows');
+Z0 = unique(allTargets{2},'rows'); 
+Z1 = unique(allTargets{3},'rows');
+Z2 = unique(allTargets{4},'rows');
 
 allTarget = {Z0 Z1 Z2};
-csvwrite('morphGoalPoint-triangle.csv',allTarget)
+csvwrite('morphGoalPoint-pentagon.csv',allTarget)
